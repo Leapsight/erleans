984be0fe1aa86ab724fbecf71d72480a40016298
Alejandro Miguez <alejandro.miguez@leapsight.com>
2025-05-29 10:55:08 -0300

    Updated max_merges to 1 instead of 3 and version_ttl to 30secs instead of 1min

--- src/erleans_pm.erl
+++ src/erleans_pm.erl
@@ -613,10 +613,10 @@ init(_) ->
         },
         %% CRDT opts
         callback_mod => ?MODULE,
-        max_merges => 3,
+        max_merges => 1,
         max_merges_per_root => 1,
         max_versions => 10,
-        version_ttl => timer:minutes(1),
+        version_ttl => timer:seconds(30),
         fwd_bcast => false,
         consistency_model => eventual
     },
